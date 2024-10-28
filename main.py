2ce9d7bfdedbf707c588a79949b00e623ec955e9
Martí Espinosa <141586888+martiespinosa@users.noreply.github.com>
2024-10-28 16:21:44 +0100

    done

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    others_num = receivedNumber
-    basic.showNumber(others_num)
-    if (my_num > others_num) {
+    basic.showNumber(receivedNumber)
+    if (my_num > receivedNumber) {
         basic.showIcon(IconNames.Happy)
-    } else if (my_num < others_num) {
+    } else if (my_num < receivedNumber) {
         basic.showIcon(IconNames.Sad)
     } else {
         basic.showIcon(IconNames.Asleep)
@@ -19,18 +18,16 @@ input.onButtonPressed(Button.B, function () {
     my_num = randint(1, 6)
     radio.sendNumber(my_num)
     basic.showNumber(my_num)
-    if (my_num > others_num) {
+    if (my_num > receivedNumber) {
         basic.showIcon(IconNames.Happy)
-    } else if (my_num < others_num) {
+    } else if (my_num < receivedNumber) {
         basic.showIcon(IconNames.Sad)
     } else {
         basic.showIcon(IconNames.Asleep)
     }
 })
-let others_num = 0
 let my_num = 0
 my_num = 0
-others_num = 0
 basic.forever(function () {
 	
 })
